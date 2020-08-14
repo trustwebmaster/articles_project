6860b5c8ec13fc90c69c4f07a87111af87d9ccc4
trust <tnusikiri43@gmail.com>
2020-08-14 04:13:54 +0200

    Finished the backe-end api end-points

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('articles', 'ArticleController@index');
+Route::get('article/{id}' , 'ArticleController@show');
+Route::post('article', 'ArticleController@store');
+Route::put('article', 'ArticleController@store');
+Route::delete('article/{id}', 'ArticleController@destroy');
